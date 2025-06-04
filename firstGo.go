4b19ec1761cd5580e4dfd734d48bc0cfd7122d90
Rodrigo Pupo <109440284+RoPupo@users.noreply.github.com>
2025-06-04 17:09:50 -0300

    - Inclui comentários detalhados em cada função, explicando de forma clara o que cada uma faz, seus parâmetros e retornos. - Acrescenta observações sobre a estrutura e organização do código, mostrando como as partes se conectam e o fluxo principal. - Torna o código mais acessível para outros desenvolvedores, ajudando na compreensão e manutenção futura.

--- firstGo.go
+++ firstGo.go
@@ -1,42 +1,74 @@
 package main // Principal pacote, código Go deve começar com o pacote main!
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func introduction() {
-	nome := "Rodrigo"
+	fmt.Println("Bem vindo ao programa de monitoramento de sites!, Por favor, digite seu nome para continuar:")
+	var nome string
+	fmt.Scan(&nome)
 	var version = 1.1
-	fmt.Println("Olá, Sr", nome, "Seja bem vindo ao programa de monitoramento de servidores!")
+	fmt.Println("Olá, Sr(a)", nome, "Seja bem vindo ao programa de monitoramento de sites!")
 	fmt.Println("Versão do programa:", float32(version))
+}
+
+func serverWeb() {
+	fmt.Println(("Digita o site a ser monitorado (exemplo: https://www.google.com):"))
+	var site string
+	fmt.Scan(&site)
+	fmt.Println("Monitorando o site:", site)
+	fmt.Println("Iniciando monitoramento do servidor web...")
+
+	resp, _ := http.Get(site) // Realiza uma requisição HTTP GET para o site informado pelo usuário. Ignorando o erro retornado com "_"
+	if resp.StatusCode == 200 {
+		fmt.Println("Site carregado com sucesso!")
+	} else {
+		fmt.Println("O site não foi carregado, verifique sua conexão ou se o site está ativo!")
+		fmt.Println("O site retornou o código de status:", resp.StatusCode)
+		os.Exit(-1)
+	}
+
+}
+
+func menu() {
+
+	fmt.Println("Por favor, selecione o tipo de servidor que deseja monitorar:")
+	fmt.Println("1 - Servidor Web")
+	fmt.Println("2 - Logs")
+	fmt.Println("3 - Sair do Programa")
+
+}
+func lercomando() int {
+	var comandolido int
+	fmt.Scan(&comandolido)
+	return comandolido
 }
 
 func main() {
 
 	introduction()
+	for {
+		menu()
+		comando := lercomando()
+		switch comando { // Switch case para verificar o comando digitado pelo usuário | Pode utilzar o if, porémo switch case é mais legível e organizado
 
-	fmt.Println("Por favor, selecione o tipo de servidor que deseja monitorar:")
-	fmt.Println("1 - Servidor Web")
-	fmt.Println("2 - Servidor de Banco de Dados")
-	fmt.Println("3 - Sair do Programa")
-
-	var comando int
-	fmt.Scan(&comando)
-	switch comando { // Switch case para verificar o comando digitado pelo usuário
-	// Pode utilzar o if, porémo switch case é mais legível e organizado
-	case 1:
-		fmt.Println("Você escolheu monitorar um servidor Web!")
-		fmt.Println("Monitorando servidor Web...")
-		// break - Não é necessário, pois o switch case já finaliza após o comando ser executado
-	case 2:
-		fmt.Println("Você escolheu monitorar um servidor de Banco de Dados!")
-		fmt.Println("Monitorando servidor de Banco de Dados...")
-	case 3:
-		fmt.Println("Obrigado por usar o programa de monitoramento de servidores!")
-		fmt.Print("Saindo do programa...")
-	default:
-		fmt.Println("Comando inválido! Por favor, selecione uma opção válida.")
+		case 1:
+			fmt.Println("Você escolheu monitorar um site Web!")
+			serverWeb()
+			// break - Não é necessário, pois o switch case já finaliza após o comando ser executado
+		case 2:
+			fmt.Println("Você escolheu visualizar os Logs...")
+		case 3:
+			fmt.Println("Obrigado por usar o programa de monitoramento de sites!")
+			fmt.Print("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Comando inválido! Por favor, selecione uma opção válida.")
+			os.Exit(-1)
+		}
 	}
-
 	// fmt.Scanf("%d", &comando) - Leitura do comando do usuário e salva na variável comando (alocado no endereço de memória)
 	//& endereço da variável comando, para que o valor lido seja armazenado nela
 }
